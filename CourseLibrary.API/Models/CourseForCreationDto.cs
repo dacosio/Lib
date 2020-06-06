ebbee321cd095bf058ec17bb2f5395b617bde0aa
cosio <ucg.don.cosio@gmail.com>
2020-06-06 19:13:45 +0800

    validation

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : CourseForManipulationDto
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
+
     }
 }
---
+++ CourseLibrary.API/ValidationAttributes/TitleDescriptionValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class TitleDescriptionValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            var course = (Models.CourseForManipulationDto)validationContext.ObjectInstance;
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult(ErrorMessage,
+                    new[] { nameof(Models.CourseForManipulationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
